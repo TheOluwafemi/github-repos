7a9bbaf1ff3a4e4997e0809a11caf1bd85381617
Theoluwafemi <oluwafemidamilola21@gmail.com>
2020-12-11 22:56:31 +0100

    chore: search page UI -updated styles

---
+++ pages/search.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="search">
+        <h3 class="text__heading">Search Github User</h3>
+        <p class="text__subheading">
+            Enter user's name, email and agree to our conditions
+        </p>
+        <article class="search__form">
+            <Form />
+        </article>
+    </section>
+</template>
+
+<script>
+export default {
+    layout: 'no-header',
+    data() {
+        return {
+            userNotFound: false,
+        }
+    },
+}
+</script>
