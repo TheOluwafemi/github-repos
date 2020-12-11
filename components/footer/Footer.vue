11e8c286192a0ba5d152fd568fabd362d353d695
Theoluwafemi <oluwafemidamilola21@gmail.com>
2020-12-11 22:56:44 +0100

    chore: added footer

---
+++ components/footer/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="footer">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quod
+        dignissimos aliquid libero consectetur sapiente dolorum accusamus optio
+        numquam cupiditate. Nulla culpa ab voluptatum exercitationem nemo
+        deserunt eveniet autem quod?
+    </div>
+</template>
