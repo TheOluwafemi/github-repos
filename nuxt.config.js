7e8c4a09307b733a3b2311c3e8c86e5ebe452cb3
Theoluwafemi <oluwafemidamilola21@gmail.com>
2020-12-14 10:39:06 +0100

    setup vuelidate for form validation

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
     css: ['@/assets/styles/main.scss'],
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: [],
+    plugins: ['~/plugins/vuelidate'],
 
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
