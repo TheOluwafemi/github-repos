6340e2d180946e846cafd1093b89e66ab912e31e
Theoluwafemi <oluwafemidamilola21@gmail.com>
2020-12-14 08:29:21 +0100

    feat: added dynamic routing
    - route to a sincle repo on clicking repo link
    - display single repo

---
+++ components/repositories/single-repo.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <pre> {{ repository }}</pre>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        repository: {
+            type: Object,
+            required: true,
+            default: () => {},
+        },
+    },
+}
+</script>
---
+++ pages/_owner/_repo.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="repo">
+        <SingleRepo :repository="repository" />
+    </section>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios, params }) {
+        const { owner } = params
+        const { repo } = params
+        const repository = await $axios.$get(`repos/${owner}/${repo}`)
+        return { repository, owner, repo }
+    },
+
+    computed: {
+        repoHeader() {
+            return `${this.owner}'s repositories`
+        },
+        // userRepos() {
+        //     return this.extractRepoDetails(this.repositories)
+        // },
+    },
+
+    mounted() {
+        console.log(typeof this.repositories)
+    },
+    methods: {
+        // filtering only repo details needed to be presented in the interface
+        // extractRepoDetails(repos) {
+        //     const allRepos = Object.keys(repos).map((key) => {
+        //         return repos[key]
+        //     })
+        //     console.log(allRepos)
+        //     const filteredRepos = allRepos.map((repo) => {
+        //         return {
+        //             id: repo.id,
+        //             name: repo.name,
+        //             fullName: repo.full_name,
+        //             url: repo.html_url,
+        //             description: repo.description,
+        //             createdAt: repo.created_at,
+        //             lastUpdated: repo.updated_at,
+        //             forks: repo.forks_count,
+        //             hasIssues: repo.has_issues,
+        //             open_issues: repo.open_issues_count,
+        //             stars: repo.stargazers_count,
+        //         }
+        //     })
+        //     return filteredRepos
+        // },
+    },
+}
+</script>
