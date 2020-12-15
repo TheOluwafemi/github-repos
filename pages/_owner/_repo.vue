b4fc1a7aa8fe359c66cfcdb7ceaaf2125ca3ee93
Theoluwafemi <oluwafemidamilola21@gmail.com>
2020-12-15 12:27:55 +0100

    feat: seo and meta tags
    - add meta tag; title, desription to each page

--- pages/_owner/_repo.vue
+++ pages/_owner/_repo.vue
@@ -30,34 +30,14 @@ export default {
         repoHeader() {
             return `${this.owner}'s repositories`
         },
-        // userRepos() {
-        //     return this.extractRepoDetails(this.repositories)
-        // },
     },
-    methods: {
-        // filtering only repo details needed to be presented in the interface
-        // extractRepoDetails(repos) {
-        //     const allRepos = Object.keys(repos).map((key) => {
-        //         return repos[key]
-        //     })
-        //     console.log(allRepos)
-        //     const filteredRepos = allRepos.map((repo) => {
-        //         return {
-        //             id: repo.id,
-        //             name: repo.name,
-        //             fullName: repo.full_name,
-        //             url: repo.html_url,
-        //             description: repo.description,
-        //             createdAt: repo.created_at,
-        //             lastUpdated: repo.updated_at,
-        //             forks: repo.forks_count,
-        //             hasIssues: repo.has_issues,
-        //             open_issues: repo.open_issues_count,
-        //             stars: repo.stargazers_count,
-        //         }
-        //     })
-        //     return filteredRepos
-        // },
+    head() {
+        return {
+            title: `${this.repository.name}| Repo`,
+            meta: [
+                { name: 'description', content: this.repository.description },
+            ],
+        }
     },
 }
 </script>
